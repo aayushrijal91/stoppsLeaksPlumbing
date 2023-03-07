cd8b5bab18bf06d39cce5460c6050e227d07e32d
Aayush Rijal <arijal@aiims.com.au>
2023-03-08 10:25:52 +1100

    404 page done

--- src/theme-files/404.php
+++ src/theme-files/404.php
@@ -2,14 +2,14 @@
 get_header();
 get_template_part('parts/section', 'banner');
 ?>
-<div class="404_page bg-light-grey py-5 py-lg-7">
+<div class="404_page py-5 py-lg-7">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-3">
                 <img src="<?= get_template_directory_uri() ?>/images/lib/no-results.png" alt="Nothing Found">
             </div>
             <div class="col-12">
-                <div class="text-center fs-50 fw-600 text-primary pt-5">Sorry, nothing found!</div>
+                <div class="text-center fs-50 fw-600 text-primary pt-5">Sorry, nothing found here!</div>
             </div>
         </div>
     </div>
